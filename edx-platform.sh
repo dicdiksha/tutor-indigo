c0f0254bd8aced8964b233cc344d881e033689f7
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2025-06-20 08:33:45 +0530

    Create edx-platform.sh

---
+++ edx-platform.sh
@@ -0,0 +1,1 @@
+tutor images build openedx --no-cache --build-arg EDX_PLATFORM_REPOSITORY=https://github.com/dicdiksha/edx-platform.git --build-arg EDX_PLATFORM_VERSION=open-release/redwood.master.ncertx
